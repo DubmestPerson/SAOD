2574c8a7548d452d9afdac773a04a77aa40759dd
Gromyko Y.S. 3012 <GYuS.20@uni-dubna.ru>
2022-09-21 13:55:26 +0300

    Gromyko Y.S. 3012 task_number_2

---
+++ SAOD2_Sem2/arrays/arrays.cpp
@@ -0,0 +1,68 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+
+//void rotate(int a[], int n) {
+//	int x = a[0];
+//	cout <<"rot"<<endl;
+//	for (int i = 1; i <= n / 2; i++) {
+//		a[i-1] = a[n - i];
+//		a[n - i] = x;
+//		x = a[i];
+//	}
+//}
+void rotate(int a[], int n) {
+	int x = a[0];
+	for (int i = 1; i < n; i++)
+		a[i - 1] = a[i];
+	a[n - 1] = x;
+}
+
+void SecondRotate(int a[], int n, bool clockwise = false) {
+	if (clockwise) {
+		int x = a[0];
+		for (int i = 1; i < n; i++)
+			a[i - 1] = a[i];
+		a[n - 1] = x;
+	}
+	else {
+		int x = a[n - 1];
+		for (int i = n - 1; i > 0; i--)
+			a[i] = a[i - 1];
+		a[0] = x;
+	}
+}
+
+int main()
+{
+	//5
+	int a[5]{ 1,2,3,4,5 };
+	for (int i = 0; i < 5; i++)
+		cout << a[i] << ' ';
+	cout << endl;
+
+	for (int* p = a; p - a < 5; p++) {
+		cout << *p << ' ';
+	}
+	cout << endl;
+
+	rotate(a, 5);
+	for (int* p = a; p - a < 5; p++)
+		cout << *p << ' ';
+	cout << endl;
+
+	SecondRotate(a, 5, true);
+	for (int* p = a; p - a < 5; p++)
+		cout << *p << ' ';
+	cout << endl;
+
+	SecondRotate(a, 5, false);
+	for (int* p = a; p - a < 5; p++)
+		cout << *p << ' ';
+	cout << endl;
+
+	return 0;
+}
+
---
+++ SAOD2_Sem2/strings/strings.cpp
@@ -0,0 +1,46 @@
+﻿
+
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+void string_copy(char* d, char* s) {
+	for (char* pd = d, *ps = s; (*pd++ = *ps++);)
+		;
+}
+int len(const char* s) {
+    int i = 0;
+    while (*s++ != 0)
+        i++;
+    return i;
+}
+
+int compare(const char* s, const char* t) {
+    while (*s != 0 && *t != 0) {
+        int c = *s - *t;
+        if (c != 0) {
+            return c;
+        }
+        s++;
+        t++;
+    }
+    return 0;
+}
+int main()
+{
+	char str[] = "Hello!";
+	cout << str << endl;
+	cout << strlen(str) << ' ' << sizeof(str) << endl;
+
+    char t[32];
+    string_copy(t, str);
+    printf("cop: %s\n", t);
+
+    printf("len: %i\n", len(str));
+    printf("compare: %i\n", compare(str, t));
+    t[0] = 'g';
+    printf("compare: %i\n", compare(str, t));
+
+    return 0;
+}
+
